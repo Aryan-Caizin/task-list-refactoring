daea3ea3dc87ab2eaf3dbd6c7d570f60ef6e68aa
Aryan-Caizin <Aryan.Raj@caizin.com>
2025-11-18 11:17:46 +0530

    Final code of Refactoring

--- src/main/java/com/codurance/training/tasks/AddCommand.java
+++ src/main/java/com/codurance/training/tasks/AddCommand.java
@@ -1,26 +1,3 @@
-//package com.codurance.training.tasks;
-//
-//import java.util.ArrayList;
-//
-//public class AddCommand {
-//    private final Project projects ;
-//        AddCommand(TaskList taskList) { this.projects = projects; }
-//
-//        @Override
-//        public void execute(String arguments) throws Exception {
-//            String[] subParts = arguments.split(" ", 2);
-//            String type = subParts[0];
-//
-//            String[] projectTask = null;
-//            if (type.equals("project")) {
-//                projects.put(subParts[1], new Tasks());
-//            } else if (type.equals("task")) {
-//                projectTask = subParts[1].split(" ", 2);
-//                projects.addProjectName(projectTask[0], projectTask[1]);
-//            }
-//        }
-//    }
-//
 
 package com.codurance.training.tasks;
 
